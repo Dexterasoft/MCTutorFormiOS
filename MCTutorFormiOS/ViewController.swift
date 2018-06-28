ee930458f00396487f60582fa2032d8f7f9d1a75
Jay Early <jxman4000@Jays-MacBook-Pro.local>
2018-06-28 17:35:40 -0400

    initial commit

---
+++ MCTutorFormiOS/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MCTutorFormiOS
+//
+//  Created by Jay Early on 6/28/18.
+//  Copyright © 2018 Dexterasoft Research. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var tutorNameTextField: UITextField!
+    @IBOutlet weak var tutorNameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Actions
+    
+
+
+}
+
